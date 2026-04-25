17047493348c88d38bde302fc553666d96b2aa79
Sk Muntazir <muntazirsk143@gmail.com>
2026-04-26 01:58:31 +0530

    Added splash and credits and menu polish

--- Assets/Ui/credits.gd
+++ Assets/Ui/credits.gd
@@ -2,5 +2,8 @@ extends Node3D
 
 func _unhandled_input(event):
 	if event.is_action_pressed("esc") or event.is_action_pressed("click"):
-		get_tree().change_scene_to_file("res://Scenes/start_menu_3d.tscn")
-		queue_free()
+		credits_finished()
+
+func credits_finished():
+	get_tree().change_scene_to_file("res://Scenes/start_menu_3d.tscn")
+	queue_free()
