d3f05d1b5b93202a4bade3525b8a0085e3ea1893
Sk Muntazir <muntazirsk143@gmail.com>
2026-04-27 05:38:49 +0530

    added phases for music

--- Scenes/tut_start.gd
+++ Scenes/tut_start.gd
@@ -8,8 +8,11 @@ func _ready():
 	Audio.fade_in_first_track(phase1)
 	await get_tree().create_timer(1.0).timeout
 	GameManager.show_dialog("Try feeding on that power source.")
+	
 
-func phase_2():
-	Audio.play_ambience(phase2)
-func phase_3():
-	Audio.play_ambience(phase3)
+
+func _on_phase_2():
+	Audio.play_ambience(phase2, 0.0)
+
+func _on_phase_3():
+	Audio.play_ambience(phase3, -6.0)
